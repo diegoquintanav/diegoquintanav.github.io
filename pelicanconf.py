117fbc7b8d18ef8b3b066846bca97d434b887b73
Diego Quintana <daquintanav@gmail.com>
2018-11-23 15:32:15 -0300

    update pelican configuration with basic stuff

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,6 @@ SITENAME = 'On the shoulders of giants'
 SITEURL = ''
 
 
-
 PATH = 'content/blog'
 
 TIMEZONE = 'Europe/Paris'
@@ -22,14 +21,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
 
@@ -42,16 +41,14 @@ MARKUP = ('md', 'ipynb')
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['ipynb.markup', 'pelican_plugins.render_math']
 
-
 # pelican-alchemy settings
 # https://github.com/nairobilug/pelican-alchemy/wiki/Settings
 
-DESCRIPTION = "A github blog, by Diego Quintana"
+SITESUBTITLE = "Diego Quintana's blog"
 HIDE_AUTHORS = True
 ICONS = (
     ('github', 'https://github.com/diegoquintanav'),
     ('linkedin', 'https://www.linkedin.com/in/diego-quintana-valenzuela/'),
+    ('stack-overflow', 'https://stackoverflow.com/users/5819113/bluesmonk'),
     ('envelope', 'mailto:daquintanav@gmail.com')
 )
-
-GOOGLE_ANALYTICS = 'UA-108898322-1'

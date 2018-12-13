98d7ce2183eedc09d932f5330e37383bfafa6a56
Diego Quintana <daquintanav@gmail.com>
2018-12-13 10:11:39 -0300

    add disqus and google analytics to publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -11,15 +11,16 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://diegoquintanav.github.io'
+SITEURL = 'https://diegoquintanav.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
+# delete old /output each time content is published
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "bluesmonk"
+GOOGLE_ANALYTICS = "UA-108898322-1"
